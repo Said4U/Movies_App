b64cd5399b9f0e8fe93b16d5aebc593ffb560f17
Shihsaid <shikhsaidov@mail.ru>
2022-10-16 16:01:05 +0300

    Измненил структуру на MVVM

---
+++ app/src/main/java/com/example/movies/viewmodel/MainActivityViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.movies.viewmodel
+
+import com.example.movies.repository.FirebaseRepository
+
+class MainActivityViewModel {
+
+    private val firebaseRepository = FirebaseRepository()
+
+    fun writeNewUser(userId: String, email: String) {
+        firebaseRepository.writeNewUser(userId, email)
+    }
+
+    fun getUser(userId: String) {
+        firebaseRepository.getUser(userId)
+    }
+}
